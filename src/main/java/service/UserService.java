ea00e6622312dd398f6898fec4de2c32a5fd6453
770292311 <770292311@qq.com>
2018-11-21 16:25:02 +0800

    hahah

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: UserService
+ * Author:   adm
+ * Date:     2018/11/21 16:06
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package service;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author adm
+ * @create 2018/11/21
+ * @since 1.0.0
+ */
+public class UserService {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
+
